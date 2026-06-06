8a6c0e02ba7501ff842db7ccce3a46db03ac59b1
HUYUSHII <yuui226@163.com>
2026-06-06 21:58:59 +0800

    chore: 添加 build.bat 一键编译脚本

---
+++ build.bat
@@ -0,0 +1,22 @@
+@echo off
+
+echo [1/3] Killing ClickFX...
+taskkill /f /im ClickFX.exe >nul 2>&1
+timeout /t 1 /nobreak >nul
+
+echo [2/3] Compiling...
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe" /target:winexe /out:ClickFX.exe /win32icon:icon.ico /reference:System.dll,System.Drawing.dll,System.Windows.Forms.dll,System.Management.dll Program.cs ClickFX.cs Config.cs Effects.cs
+
+if errorlevel 1 (
+    echo.
+    echo Build FAILED!
+    pause
+    exit /b 1
+)
+
+echo [3/3] Starting ClickFX...
+start "" "%~dp0ClickFX.exe"
+
+echo.
+echo Build OK - ClickFX is running
+timeout /t 2 >nul
